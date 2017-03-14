cc11d5ddfbb5a3d7348dda0f9693a6c64104b313
Karen Y Ng <karen.y.ng@intel.com>
2017-03-14 09:04:43 -0700

    Change how the range of years of files to download

--- src/download_data.py
+++ src/download_data.py
@@ -10,8 +10,10 @@ import os
 dataDir = "../data"
 if not os.path.exists(dataDir):
     os.mkdir(dataDir)
+year_range = (1987, 2009)
 
-for year in range(1987, 2009):  # 2009
+for year in range(*year_range):  # 2009
+    print ("range of years of files to download = ", year_range)
     print ("downloading data from year {0} to {1}/{0}.csv.bz2".format(
         year, dataDir
     ))
