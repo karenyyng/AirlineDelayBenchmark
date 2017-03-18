ac6cc3ca914e65dff666c57f317af0058dba2f05
karenyyng <karen.yyng@gmail.com>
2017-03-18 11:15:42 -0700

    Fix print statements to be Python 3 compliant

--- src/read_csv_and_print_stat.py
+++ src/read_csv_and_print_stat.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
-
+"""
+uses python 3
+"""
 import pandas as pd
 import numpy as np
 
 data_path = "../data/"
 
 # First read in the data from 1987 to 2007
-year = [ str(i) for i in range(1987, 2008) ]
+year = [str(i) for i in range(1987, 2008)]
 # create empty dataframe
 delay1 = pd.DataFrame()
 # loop through the year-by-year csvs
 for yr in year:
     # read in relevant column from csv file using pandas
-    file = yr +'.csv'
+    file = yr + '.csv'
     temp = pd.read_csv(data_path + file, usecols=["ArrDelay"])
     # append the dataframes - this is done by reference not by value
     delay1 = delay1.append(temp)
-    print 'appending '+ file + ' - total lines = ' + \
-            '{0}'.format(delay1.shape[0])
+    print('appending ' + file + ' - total lines = ' +
+          '{0}'.format(delay1.shape[0]))
 
 # create another empty dataframe for handling month by month csv
 # delay2 = pd.DataFrame()
@@ -46,11 +48,11 @@ delay = pd.DataFrame(delay1)
 print 'total number of valid lines = {0}'.format(delay.dropna().shape[0])
 
 # note that pandas ignores nans automatically while computing stats
-#print 'mean = {0} \n'.format(delay[0].mean()) +\
-#    'median = {0} \n'.format(delay[0].median()) +\
-#    'std. dev. = {0}\n'.format(delay[0].std())
+#print('mean = {0} \n'.format(delay[0].mean()) +
+#    'median = {0} \n'.format(delay[0].median()) +
+#    'std. dev. = {0}\n'.format(delay[0].std()))
 
-print 'saving to results2.txt'
+print('saving to results2.txt')
 f = open('results2.txt', 'w')
 f.write('mean = {0}\n'.format(delay[0].mean()))
 f.write('median = {0}\n'.format(delay[0].median()))
