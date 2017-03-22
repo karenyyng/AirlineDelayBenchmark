bbcd3d92d6809597bd766187ddb33aeaa3d064f0
Karen Y Ng <karen.y.ng@intel.com>
2017-03-21 17:06:38 -0700

    add compression option to script

--- src/convert_csv_to_hdf5.py
+++ src/convert_csv_to_hdf5.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
+"""
+This uses Python 3
+Does not work on Windows
+"""
 
 import pandas as pd
 
 data_path = "../data"
 year_range = (1987, 2009)
+compression = 'gzip'
 
-# for year in range(2000, 2009):
 for year in range(*year_range):
-    print("converting the file {0}/{1}.csv to {0}/{1}.h5".format(data_path, year) )
-    df = pd.read_csv(data_path + "/" + str(year) + ".csv", encoding="ISO-8859-1")
-    df.to_hdf(data_path + "/" + str(year) + ".h5", key="csv")
+    filename = data_path + "/" + str(year) + '_' + compression + '.h5'
+    print("converting the file {0}/{1}.csv to {2}".format(
+        data_path, year, filename))
+    df = pd.read_csv(data_path + "/" + str(year) + ".csv",
+                     encoding="ISO-8859-1")
+    df.to_hdf(filename, key="csv")
