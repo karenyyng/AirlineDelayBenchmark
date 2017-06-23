ef3ca1b0a8ab5bebc14c5a8df5aebf52f3c5cfde
Karen Y. Ng <karen.y.ng@intel.com>
2017-06-23 12:54:00 -0400

    Remove compression denotion in filename to avoid confusion

--- src/convert_csv_to_hdf5.py
+++ src/convert_csv_to_hdf5.py
@@ -8,12 +8,16 @@ import pandas as pd
 
 data_path = "../data"
 year_range = (1987, 2009)
-compression = 'gzip'
+compression = None
 
 for year in range(*year_range):
-    filename = data_path + "/" + str(year) + '_' + compression + '.h5'
+    if compression is None:
+        filename = data_path + "/" + str(year) + '.h5'
+    else:
+        filename = data_path + "/" + str(year) + '_' + compression + '.h5'
+
     print("converting the file {0}/{1}.csv.bz2 to {2}".format(
         data_path, year, filename))
     df = pd.read_csv(data_path + "/" + str(year) + ".csv.bz2",
                      encoding="ISO-8859-1")
-    df.to_hdf(filename, key="csv")
+    df.to_hdf(filename, key="csv", format='table', complib=compression)
