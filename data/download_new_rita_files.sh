b88cd4da0269a08d604bcf7cf74d327247c76736
Karen Y. Ng <karen.y.ng@intel.com>
2017-06-27 13:50:05 -0400

    Update script for downloading 69 GB of raw csv data

--- data/download_new_rita_files.sh
+++ data/download_new_rita_files.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
+# requires: unzip, wget  
+# we download the raw data from 
+# https://www.transtats.bts.gov/DL_SelectFields.asp?Table_ID=236&DB_Short_Name=On-Time
 
-### if you only use n_process = 8 and you have 16 files, 
-# only 8 of them will be processed with this script...
 n_process=6
 cat ./rita_file_paths.txt | xargs -n 1 -P $n_process wget 
+ls *.zip | xargs -n 1 -P $n_process unzip
+rm *.zip 
+python rename_files.py
