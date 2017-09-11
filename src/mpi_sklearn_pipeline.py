6a9640e8074c8766ed1cc4c0b76afb655ebc2985
Karen Y. Ng <karen.y.ng@intel.com>
2017-09-11 11:24:15 -0700

    Show how to correctly use the gather operation after Robert's help

--- src/mpi_sklearn_pipeline.py
+++ src/mpi_sklearn_pipeline.py
@@ -5,9 +5,11 @@ Code to perform parallel io via MPI
 from mpi4py import MPI
 # import numpy as np
 import pandas as pd
+import numpy as np
 import time
 
 # import scripts that Karen wrote
+# import serial_preprocess_data as preprocess
 import utils
 
 h5list = utils.getFileList("../data/", "h5")
@@ -33,10 +35,23 @@ columns = ['Year',
            'DayOfWeek'
            ]
 
-df = pd.read_hdf(h5list[rank], columns=columns)
+df = pd.read_hdf("../data/" + h5list[rank], columns=columns)
 print("Rank {} has finished reading in h5 file".format(rank))
+start_time = time.time()
+df_list = comm.gather(df, root=0)
+end_time = time.time()
+
 if rank == 0:
+    print("Size of files going through IO is {0:.2f} GB".format(
+        utils.getFileSizeInGB(
+            ["../data/" + h5file for h5file in h5list[:workers]])))
+    print("Reading in {0} selected columns only".format(len(columns)))
+    print("Gathering read df took {0:.0f} s".format(end_time - start_time))
     start_time = time.time()
-    df_list = comm.gather(df, root=0)
+    df = pd.concat(df_list)
     end_time = time.time()
-    print("Gathering read df took {} s".format(start_time - end_time))
+    print("Concatenating df took {0:.0f} s".format(end_time - start_time))
+    print("Memory usage of the data frame is {0:.2f} GB".format(
+        np.sum(df.memory_usage()) / 1e9))
+    print("Shape of df is ", df.shape)
+    print("Dtype of df is ", df.dtypes)
