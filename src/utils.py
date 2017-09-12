73050a59e34059be4c05e112ab377fc0eaec7025
karenyyng <karen.yyng@gmail.com>
2017-09-12 15:42:25 -0700

    Add utilities

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@
 General utility functions for understanding the data
 """
 import os
+import subprocess
 
 
 def getFileSizeInGB(file_list):
@@ -17,3 +18,12 @@ def getFileSizeInGB(file_list):
 def getFileList(data_dir, file_format):
     return [data_file for data_file in os.listdir(data_dir)
             if data_file.endswith(file_format)]
+
+
+def capture_multiline_output(command):
+    try:
+        return str(subprocess.check_output(command, shell=True,
+                                           stderr=subprocess.STDOUT)
+                   ).split('\\n')
+    except:
+        return ''
