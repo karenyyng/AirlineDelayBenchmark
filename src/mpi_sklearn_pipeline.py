5270bbbde0b5a3ba7c63f400e0364c89662653d0
karenyyng <karen.yyng@gmail.com>
2017-09-12 15:59:43 -0700

    Add code for saving timing info to json file

--- src/mpi_sklearn_pipeline.py
+++ src/mpi_sklearn_pipeline.py
@@ -7,6 +7,7 @@ from mpi4py import MPI
 import pandas as pd
 import numpy as np
 import time
+import datetime
 
 # import scripts that Karen wrote
 # import serial_preprocess_data as preprocess
@@ -35,23 +36,72 @@ columns = ['Year',
            'DayOfWeek'
            ]
 
+start_time = time.time()
 df = pd.read_hdf("../data/" + h5list[rank], columns=columns)
-print("Rank {} has finished reading in h5 file".format(rank))
+end_time = time.time()
+
+print("Worker {0} has finished reading in h5 file in {1:.2f}s".format(
+    rank, end_time - start_time))
+
+read_df_time = comm.gather(end_time - start_time, root=0)
+
 start_time = time.time()
 df_list = comm.gather(df, root=0)
 end_time = time.time()
 
+gather_time = comm.gather(end_time - start_time, root=0)
+print("Worker {0} has gathered h5 file in {1:.2f}s".format(
+    rank, end_time - start_time))
+
 if rank == 0:
-    print("Size of files going through IO is {0:.2f} GB".format(
-        utils.getFileSizeInGB(
-            ["../data/" + h5file for h5file in h5list[:workers]])))
-    print("Reading in {0} selected columns only".format(len(columns)))
-    print("Gathering read df took {0:.0f} s".format(end_time - start_time))
+    verbose = True
+    save_json = True
+
+    timing_info = {}
+    timing_info['read_hdf_seconds'] = \
+        np.array([round(read_time, 2) for read_time in read_df_time])
+    timing_info['gather_hdf_seconds'] = \
+        np.array([round(read_time, 2) for read_time in gather_time])
     start_time = time.time()
     df = pd.concat(df_list)
     end_time = time.time()
-    print("Concatenating df took {0:.0f} s".format(end_time - start_time))
-    print("Memory usage of the data frame is {0:.2f} GB".format(
-        np.sum(df.memory_usage()) / 1e9))
-    print("Shape of df is ", df.shape)
-    print("Dtype of df is ", df.dtypes)
+
+    timing_info['df.concat_seconds'] = round(end_time - start_time, 2)
+    timing_info['file_size_GB'] = round(utils.getFileSizeInGB(
+            ["../data/" + h5file for h5file in h5list[:workers]]), 2)
+    timing_info['df_size_GB'] = round(np.sum(df.memory_usage()) / 1e9, 2)
+    timing_info['median_read_gather_concat_time_seconds'] = \
+        np.median(timing_info['read_hdf_seconds'] +
+                  timing_info['gather_hdf_seconds']
+                  ) + timing_info['df.concat_seconds']
+    timing_info['file_IO_MB_per_second'] = \
+        round(timing_info['file_size_GB'] * 1000 /
+              timing_info['median_read_gather_concat_time_seconds'], 2)
+
+    timing_info['df_IO_MB_per_second'] = \
+        round(timing_info['df_size_GB'] * 1000 /
+              timing_info['median_read_gather_concat_time_seconds'], 2)
+
+    # timing_info['IO_GB_per_second'] =
+    timing_info['df_shape'] = df.shape
+    timing_info['date'] = time.strftime('%Y-%m-%d-%H-%M-%S')
+    timing_info['git_commit'] = utils.capture_multiline_output(
+        'git show | head -1')
+    timing_info['git_repo'] = utils.capture_multiline_output(
+        'git remote -v ')
+    timing_info['conda_env'] = utils.capture_multiline_output(
+        'conda list')
+    timing_info['read_hdf_seconds'] = \
+        list(timing_info['read_hdf_seconds'])
+    timing_info['gather_hdf_seconds'] = \
+        list(timing_info['gather_hdf_seconds'])
+
+    if verbose:
+        for k in timing_info:
+            print("{0} = {1}".format(k, timing_info[k]))
+
+    if save_json:
+        import json
+        with open("../slurm_logs/mpi4py_io_perf_{}.json".format(
+                timing_info['date']), 'w') as f:
+            json.dump(timing_info, f)
