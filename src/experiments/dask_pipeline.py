57cfe4d0f394384b2677b8e52faa26e3c42d0b29
karenyyng <karen.yyng@gmail.com>
2017-09-21 16:56:16 -0700

    Add timing output for dask pipeline file

--- src/experiments/dask_pipeline.py
+++ src/experiments/dask_pipeline.py
@@ -5,10 +5,10 @@ import sys
 sys.path.append("../")
 import argparse
 from distributed import Executor, Client, LocalCluster
-import dask
 import dask.dataframe as ddf
-import os
+import numpy as np
 import psutil
+import time
 
 # import my utils
 import utils
@@ -31,42 +31,95 @@ columns = ['Year',
            ]
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Launch dask workers on one single node to read files."
-    )
-    cpu_count = psutil.cpu_count()
-    print("Max thread count from python is", cpu_count)
+    # default values of parameters
+    data_dir = "../../data/"
+    log_dir = "../../slurm_logs/"
+    no_of_files = 20
     default_no_of_threads = 1
+    cpu_count = psutil.cpu_count()
     if cpu_count // 4 - 2 > 1:
         default_no_of_workers = cpu_count // 4 - 2
     else:
         default_no_of_workers = 2
-    h5filelist = utils.getFileList("../../data/", 'h5')
-    no_of_files = 20
-    print("File size to read in is {0:.0f} GB".format(
-          utils.getFileSizeInGB(h5filelist[:no_of_files])))
+    print("Max thread count from python is", cpu_count)
+    dec_pt = 2
+
+    parser = argparse.ArgumentParser(
+        description="Launch dask workers on one node to read files."
+    )
     parser.add_argument("--n_workers", default=default_no_of_workers, type=int,
                         help="number of dask workers "
                         )
     parser.add_argument("--n_threads", default=default_no_of_threads, type=int,
-                        help="number of threads per workers "
+                        help="number of threads per worker"
                         )
-    parser.add_argument("--n_file", default=no_of_files, type=int,
+    parser.add_argument("--n_files", default=no_of_files, type=int,
                         help="number of files in total to read in"
+                        )
+    parser.add_argument("--data_dir", default=data_dir, type=str,
+                        help="path to the data dir, do not include file name"
+                        )
+    parser.add_argument("--output_json", default=True, type=bool,
+                        help="bool, whether to save a log for timing"
                         )
 
     args = parser.parse_args()
     n_workers = args.n_workers
     n_threads = args.n_threads
+    data_dir = args.data_dir
+    no_of_files = args.n_files
+
+    if no_of_files < n_workers:
+        raise ValueError("n_files = {0} > n_workers {1}".format(
+            no_of_files, n_workers))
+
+    # start script
+    h5filelist = sorted(utils.getFileList(data_dir, 'h5'))
+    # print(h5filelist[:no_of_files])
+    file_size = utils.getFileSizeInGB(h5filelist[:no_of_files])
+    print("File size to read in is {0:.0f} GB".format(file_size))
+
     cluster = LocalCluster(n_workers=n_workers, threads_per_worker=n_threads)
     client = Client(cluster)
     print("Using {0} workers and {1} thread(s) per worker".format(
         n_workers, n_threads))
-    print("Reading in {} files".format(no_of_files))
     df = ddf.read_hdf(h5filelist[:no_of_files], 'csv', columns=columns)
-    print(df.compute().shape)
+    start_time = time.time()
+    df = client.persist(df)
+    print(df.index.compute())
+    end_time = time.time()
+    read_time = end_time - start_time
+    # print("lines read in =", df.shape)
+    print("Reading in {0} files in {1:.0f}s ".format(no_of_files, read_time))
 
+    df_size = np.sum(df.memory_usage().compute()) / 1e9
 
+    if args.output_json:
+        from collections import OrderedDict
+        import json
+        timing_info = OrderedDict({})
+
+        timing_info['git_commit'] = utils.capture_multiline_output(
+            'git show | head -1')[0]
+        timing_info['git_repo'] = utils.capture_multiline_output(
+            'git remote -v ')[0]
+        timing_info['conda_env'] = utils.capture_multiline_output(
+            'conda list')
+        timing_info['lscpu'] = utils.capture_multiline_output(
+            'lscpu')[0]
+        timing_info['date'] = time.strftime('%Y-%m-%d-%H-%M-%S')
+
+        timing_info['read_hdf_seconds'] = round(read_time, dec_pt)
+        timing_info['file_IO_MB_per_second'] = \
+            round(file_size * 1000 / read_time, dec_pt)
+        timing_info['df_IO_MB_per_second'] = \
+            round(df_size * 1000 / read_time, dec_pt)
+        json_file = log_dir + "dask_io_perf_{}.json".format(
+                timing_info['date'])
+        print("outputting timing to ", json_file)
+        with open(json_file, 'w') as f:
+            json.dump(timing_info, f)
+        for k in timing_info.keys():
+            print("{0} = {1}".format(k, timing_info[k]))
 
     # df = df.dropna()
-    # print(df.describe().compute())
