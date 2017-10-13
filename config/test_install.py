2abfb474aa4000f59a11e8bcdcf05d3568af9e4d
Karen Ng <karen.y.ng@intel.com>
2017-10-13 16:51:34 -0700

    Add script to test whether python libraries were installed successfully or not

---
+++ config/test_install.py
@@ -0,0 +1,13 @@
+"""
+Usage: 
+source ./load_py*.sh
+python test_install.py
+"""
+import tables 
+import numpy 
+import pandas 
+import mpi4py
+import distributed 
+import matplotlib
+import h5py
+print("Environment seemed to be set up properly")
